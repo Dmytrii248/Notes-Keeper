ffe1d006ee111e5a699a107324c10014fcbd5e1a
Dmytro <hrab.dmytrii@gmail.com>
2025-02-10 20:37:56 +0200

    fix: remove rxdb dev pluggin

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,5 @@
 import { addRxPlugin, createRxDatabase, RxDatabase } from "rxdb";
 import { getRxStorageDexie } from "rxdb/plugins/storage-dexie";
-import { RxDBDevModePlugin } from "rxdb/plugins/dev-mode";
 import { wrappedValidateAjvStorage } from "rxdb/plugins/validate-ajv";
 import { RxDBUpdatePlugin } from "rxdb/plugins/update";
 import { noteSchema, TDatabaseCollections } from "./models";
@@ -8,19 +7,15 @@ import { noteSchema, TDatabaseCollections } from "./models";
 let dbPromise: Promise<RxDatabase<TDatabaseCollections>> | null = null;
 
 const _create = async () => {
-  addRxPlugin(RxDBDevModePlugin);
   addRxPlugin(RxDBUpdatePlugin);
 
-  console.log("DatabaseService: creating database..");
   const db = await createRxDatabase<TDatabaseCollections>({
     name: "notesreactdb",
     storage: wrappedValidateAjvStorage({ storage: getRxStorageDexie() }),
   });
 
-  console.log("DatabaseService: created database");
   (window as unknown as { db: RxDatabase<TDatabaseCollections> }).db = db;
 
-  console.log("DatabaseService: create collections");
   await db.addCollections({
     notes: {
       schema: noteSchema,
